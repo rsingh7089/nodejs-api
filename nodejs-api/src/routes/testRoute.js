47fe9df9071c46427f272f81870cd70c1fada11d
Nitininfograins <nitinmalviya@infograins.com>
2025-04-17 11:31:07 +0530

    Delete nodejs-api directory

--- nodejs-api/src/routes/testRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/test", (req, res) => {
-  res.json({ message: "This is a test API endpoint" });
-});
-
-module.exports = router;
